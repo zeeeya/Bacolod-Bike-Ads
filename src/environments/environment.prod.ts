e8209097a77fe570ad555b0ff8cfe744d4dce05e
zeeeya <zci.alvarez@gmail.com>
2018-02-15 22:05:26 +0800

    Firestarter ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDvooBWfjinY5uAGJ2xgRbeFcn1znJCUWo",
+    authDomain: "bacolodbikeads.firebaseapp.com",
+    databaseURL: "https://bacolodbikeads.firebaseio.com",
+    projectId: "bacolodbikeads",
+    storageBucket: "",
+    messagingSenderId: "860955167884"
+
+    }
+};
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyDvooBWfjinY5uAGJ2xgRbeFcn1znJCUWo",
+    authDomain: "bacolodbikeads.firebaseapp.com",
+    databaseURL: "https://bacolodbikeads.firebaseio.com",
+    projectId: "bacolodbikeads",
+    storageBucket: "",
+    messagingSenderId: "860955167884"
+
     }
 };
